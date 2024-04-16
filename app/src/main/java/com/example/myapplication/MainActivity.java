67da6b14f2f6cb1f63ba8349632ae5e9075c5a65
q <q@q.q>
2024-04-16 10:50:06 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String text = "";
+    private char[] textChar = new char[text.length()];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText email = (EditText)findViewById(R.id.editTextTextEmailAddress);
+        email.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                text = email.getText().toString();
+                for (int i = 0; i < text.length(); i++){
+                    if (text.charAt(i) == '@'){
+                        email.setTextColor(Color.BLACK);
+                        for (int j = 0; j < text.length(); j++){
+                            if (text.charAt(j) == '.'){
+                                email.setTextColor(Color.GREEN);
+                            }
+                        }
+                    }
+                }
+            }
+
+        });
+
+        EditText phone = (EditText)findViewById(R.id.editTextNumber);
+        email.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                text = phone.getText().toString();
+                for (int i = 0; i < text.length(); i++){
+                    if (text.charAt(i) == ' '){
+                        phone.setTextColor(Color.BLACK);
+                        for (int j = 0; j < text.length(); j++){
+                            if (text.charAt(j) == ' '){
+                                phone.setTextColor(Color.GREEN);
+                            }
+                        }
+                    }
+                }
+            }
+
+        });
+
+    }
+}
